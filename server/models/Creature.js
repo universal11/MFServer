c844772336f2836c29b21533b5f0ca5bd3a6b763
Marc Martinez <universal11@Marcs-MacBook-Pro.local>
2014-06-11 07:10:49 -0700

    Removed old directories

--- server/models/Creature.js
+++
@@ -1,6 +0,0 @@
-function Creature(){
-
-}
-
-
-module.exports = Creature;
--- server/models/index.js
+++
@@ -1,4 +0,0 @@
-module.exports.Creature = require("./Creature.js");
-module.exports.Ability = require("./Ability.js");
-module.exports.Player = require("./Player.js");
-module.exports.Battle = require("./Battle.js");
